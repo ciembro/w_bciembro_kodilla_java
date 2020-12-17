e9a3a0d90861acc2d0bac2f62fd5d359e7810af4
ciembro <w.ciembroniewicz@gmail.com>
2020-12-17 14:36:54 +0100

    7.2 added ForumUser class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,17 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
+
+        //7.2
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
         //ZADANIE 7.1
         PoemBeautifier poemBeautifier = new PoemBeautifier();
