67821a9d1f93bbe13d92dad2c4a77646be5af09f
ciembro <w.ciembroniewicz@gmail.com>
2021-04-23 13:20:05 +0200

    aspect

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeAspect.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class OrderFacadeAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeAspect.class);
+
+    @Before(value = "execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+                "&& target(object)")
+    void logExecution(JoinPoint joinPoint, Object object){
+        LOGGER.info("Class: " + object.getClass().getName() + ": running " + joinPoint.getSignature().getName() );
+    }
+
+}
